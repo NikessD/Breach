a3b6cf1b444c959f0fcd56c00ad02ec1a68ae88c
Nikess <k.mauer@seznam.cz>
2026-02-18 19:55:57 +0100

    Add audio bus layout and update menu UI
    Introduce a new AudioBusLayout resource and point project.godot to it; adjust default_bus_layout.tres bus name/send. Update menu scene layout (ColorRect offsets) and add a new "SOUNDS" label in Settings. Modify volume_slider.tscn to include a Theme subresource and tweak slider layout/behavior (offsets, ticks, scrollable). In scripts/menu.gd add brief debug prints of GlobalVars.volume in _ready and remove several exit debug prints; retain loading/saving and slider volume initialization.

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -11,9 +11,15 @@ var progress: = 0.0
 
 func _ready() -> void:
 	Saveload._load()
+	print("----------------------")
+	print(str(GlobalVars.volume[0]))
+	print(str(GlobalVars.volume[1]))
 	GlobalVars.night_number = Saveload.contents_to_save.night_number
 	GlobalVars.volume[1] = Saveload.contents_to_save.vfx_volume
 	GlobalVars.volume[0] = Saveload.contents_to_save.master_volume
+	print("----------------------")
+	print(str(GlobalVars.volume[0]))
+	print(str(GlobalVars.volume[1]))
 	slider_volume_master._set_volume()
 	slider_volume_vfx._set_volume()
 	$Menu/MenuButtons/Play.text = "NIGHT " + str(GlobalVars.night_number)
@@ -110,14 +116,7 @@ func _on_exit_pressed() -> void:
 	Saveload.contents_to_save.master_volume = GlobalVars.volume[0] 
 	Saveload.contents_to_save.vfx_volume = GlobalVars.volume[1] 
 	Saveload._save()
-	print(str(slider_volume_master.bus_index))
-	print(str(abs(slider_volume_vfx.bus_index)))
-	print("----------------------")
-	print(str(Saveload.contents_to_save.master_volume))
-	print(str(Saveload.contents_to_save.vfx_volume))
-	print("----------------------")
-	print(str(GlobalVars.volume[0]))
-	print(str(GlobalVars.volume[1]))
+
 
 
 func _on_exit_mouse_entered() -> void:
